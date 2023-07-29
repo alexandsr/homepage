29a99283c3156d6490228d4dd4fddd54f4e0e18a
levtus <61996958+alexandsr@users.noreply.github.com>
2023-07-29 19:28:10 +0200

    Fix drop shadows, add icons, etc.

---
+++ acknowledgements.md
@@ -0,0 +1,6 @@
+[Font Awesome 6 Icons](https://react-icons.github.io/react-icons/icons?name=fa6) used 
+
+
+Fonts from Google Fonts
+[Tilit Warp](https://fonts.google.com/specimen/Tilt+Warp)
+[Sirin Stencil](https://fonts.google.com/specimen/Sirin+Stencil)
---
+++ build/buttons.js
@@ -0,0 +1,12 @@
+tippy('#linkedin', {
+    content: 'LinkedIn - in/alexander-weimer',
+});
+tippy('#github', {
+    content: 'GitHub - @alexandsr',
+});
+tippy('#medium', {
+    content: 'Medium.com - @alexsnder',
+});
+tippy('#email', {
+    content: 'Email - alexander.weimer@proton.me',
+});
