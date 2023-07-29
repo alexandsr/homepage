cabde61f2936525db5f1f73f8dcee43616b756fb
levtus <61996958+alexandsr@users.noreply.github.com>
2023-07-29 13:35:56 +0200

    Fix links in html

---
+++ src/text.ts
@@ -0,0 +1,36 @@
+if (window.matchMedia("(min-width: 768px)").matches) {
+  // The place I saw this effect:
+  // https://angle2.agency/
+
+  // Red Staper made a NICE tutorial about this effect! You should check it out!
+  // https://www.youtube.com/watch?v=LgiadQQM6mo&t=5s
+
+  window.addEventListener('mousemove', handleMouseMove);
+  window.addEventListener('resize', handleWindowResize);
+
+  const spansSlow = document.querySelectorAll('.spanSlow');
+  const spansMed = document.querySelectorAll('.spanMed');
+  const spansFast = document.querySelectorAll('.spanFast');
+
+  let width = window.innerWidth;
+
+  function handleMouseMove(e) {
+    let normalizedPosition = e.pageX / (width/2) - 1;
+    let speedSlow = 75 * normalizedPosition;
+    let speedMed = 175 * normalizedPosition;
+    let speedFast = 250 * normalizedPosition;
+    spansSlow.forEach((span) => {
+      (span as HTMLElement).style.transform = `translate(${speedSlow}px)`;
+    });
+    spansMed.forEach((span) => {
+      (span as HTMLElement).style.transform = `translate(${speedMed}px)`
+    });
+    spansFast.forEach((span) => {
+      (span as HTMLElement).style.transform = `translate(${speedFast}px)`
+    });
+  }
+  //we need to recalculate width when the window is resized
+  function handleWindowResize() {
+    width = window.innerWidth;
+  }
+}
